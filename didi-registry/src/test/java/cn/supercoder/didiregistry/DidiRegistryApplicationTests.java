e6819e6f9f605faa9a1013206e2e44ee51f847f4
Yuxiang Zheng <yuxiang@celential.ai>
2021-01-26 11:54:00 +0800

    Minimize configuration

---
+++ didi-registry/src/test/java/cn/supercoder/didiregistry/DidiRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.supercoder.didiRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DidiRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
